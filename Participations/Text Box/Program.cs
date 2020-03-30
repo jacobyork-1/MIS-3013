3f08fceaaacb2e50bc6f55c7440c6c743363a537
Jacob York <Jacob.C.York-1@ou.edu>
2020-03-30 13:06:58 -0500

    working on the participation

---
+++ Participations/Text Box/Program.cs
@@ -0,0 +1,25 @@
+﻿//Jacob York
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Box
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox textbox1 = new TextBox();
+            textbox1.BackGround = ConsoleColor.DarkCyan;
+            textbox1.ForeGround = ConsoleColor.Blue;
+            textbox1.Padding = 3;
+            textbox1.Text = "This is a the first text box.";
+
+            textbox1.DisplayText();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participations/Text Box/TextBox.cs
@@ -0,0 +1,34 @@
+﻿//Jacob York
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Text_Box
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+
+        public ConsoleColor BackGround { get; set; }
+
+        public ConsoleColor ForeGround { get; set; }
+
+        public string Text { get; set; }
+
+        public TextBox()
+        {
+
+        }
+
+        public void DisplayText()
+        {
+            Console.ForegroundColor = this.ForeGround;
+            Console.BackgroundColor = this.BackGround;
+            string textOutput = new string(' ', this.Padding) + this.Text + new string(' ', this.Padding);
+            Console.WriteLine(textOutput);
+        }
+
+    }
+}

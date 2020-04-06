a7a3d636a9c907f0da86a12eb92cdb945edfb9a7
Jacob York <Jacob.C.York-1@ou.edu>
2020-04-06 17:47:57 -0500

    Final changes

---
+++ Participations/Classes - Student1/Program.cs
@@ -0,0 +1,44 @@
+﻿//Jacob York
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes___Student1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name.");
+            string FN = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name.");
+            string LN = Console.ReadLine();
+
+            Student Student = new Student
+            {
+                FirstName = "",
+                LastName = "",
+            };
+
+            Console.WriteLine("Please enter the course name. (e.g. MIS3013, MIS3033)");
+            CourseNames = Console.ReadLine();
+            List<string> CourseNames = new List<string>
+            {
+                Student
+            };
+
+            Console.WriteLine("Please enter your corresponding grades for each course. (e.g. A, B, C, D, F)");
+            CourseGrades = Console.ReadLine();
+            List<string> CourseGrades = new List<string>
+            {
+                Student
+            };
+
+            Console.WriteLine($"The GPA for {Student.FirstName} {Student.LastName} is {Student.CalculateGPA}.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participations/Classes - Student1/Student.cs
@@ -0,0 +1,63 @@
+﻿//Jacob York
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes___Student1
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public List<string> CourseNames { get; set; }
+
+        public List<string> CourseGrades { get; set; }
+
+
+        public Student()
+        {
+            FirstName = "";
+            LastName = "";
+            List<string> CourseNames = new List<string>();
+            List<string> CourseGrades = new List<string>();
+        }
+
+        public Student(string first, string second)
+        {
+            FirstName = first;
+            LastName = second;
+            List<string> CourseNames = new List<string>();
+            List<string> CourseGrades = new List<string>();
+        }
+
+        public double CalculateGPA()
+        {
+            double TotalPointsEarned = 1;
+            double TotalCreditsAttempted = 1;
+
+            double GradePoints = 0;
+
+
+            switch (LetterGrade)
+            {
+                case 'A':
+                    GradePoints = 4; break;
+                case 'B':
+                    GradePoints = 3; break;
+                case 'C':
+                    GradePoints = 2; break;
+                case 'D':
+                    GradePoints = 1; break;
+                case 'F':
+                    GradePoints = 0; break;
+            }
+            return GradePoints;
+
+            return TotalPointsEarned / TotalCreditsAttempted;
+        }
+    }
+}

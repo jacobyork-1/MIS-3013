bd79fb4c42a976523e83a95795d7302459c0e025
Jacob York <Jacob.C.York-1@ou.edu>
2020-04-20 13:18:30 -0500

    working on WPF - Student

---
+++ Participations/WPF - Student/Student.cs
@@ -0,0 +1,28 @@
+﻿//Jacob York
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF___Student
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FavoriteColor { get; set; }
+        public Student()
+        {
+            StudentID = 0;
+            FirstName = "";
+            LastName = "";
+            FavoriteColor = "";
+        }
+        public string ToString()
+        {
+            Console.WriteLine(FirstName + LastName + " (" + StudentID + ") " + "has a favoite color of " + FavoriteColor);
+        }
+    }
+}
